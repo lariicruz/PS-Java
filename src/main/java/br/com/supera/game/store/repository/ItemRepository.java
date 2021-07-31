dcdd9a67d2dcbef661ceb7a8c529e4f4591ff1b9
Larissa Cruz <mlp-larissa@hotmail.com>
2021-07-31 14:55:57 -0300

    adicao dos repository de item, product e shoppingCart

---
+++ src/main/java/br/com/supera/game/store/repository/ItemRepository.java
@@ -0,0 +1,12 @@
+package br.com.supera.game.store.repository;
+
+
+import br.com.supera.game.store.model.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemRepository extends JpaRepository<Item,Integer> {
+
+
+}
