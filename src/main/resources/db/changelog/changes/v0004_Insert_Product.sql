eaa51b2a811ec0111b1e6ce2b852ef23dab5f11a
Larissa Cruz <mlp-larissa@hotmail.com>
2021-08-01 16:26:19 -0300

    adicao do liquibase e atualizacao da collection do postman

---
+++ src/main/resources/db/changelog/changes/v0004_Insert_Product.sql
@@ -0,0 +1,10 @@
+INSERT INTO public.product (image,name,price,score) VALUES
+	 ('super-mario-odyssey.png','Super Mario Odyssey',197.88,100),
+	 ('call-of-duty-infinite-warfare.png','Call Of Duty Infinite Warfare',49.99,80),
+	 ('the-witcher-iii-wild-hunt.png','The Witcher III Wild Hunt',119.50,250),
+	 ('call-of-duty-wwii.png','Call Of Duty WWII',249.99,205),
+	 ('mortal-kombat-xl.png','Mortal Kombat XL',69.99,150),
+	 ('shards-of-darkness.png','Shards of Darkness',71.94,400),
+	 ('terra-media-sombras-de-mordor.png','Terra Média: Sombras de Mordor',79.99,50),
+	 ('fifa-18.pn','FIFA 18',195.39,325),
+	 ('horizon-zero-dawn.png','Horizon Zero Dawn',115.80,290);
